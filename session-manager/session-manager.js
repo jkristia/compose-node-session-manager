fb4d3ee069671d24588204b0994f4072d8d2cfa2
Jesper <jkristia@outlook.com>
2024-11-17 11:23:49 -0800

    .

--- session-manager/session-manager.js
+++ session-manager/session-manager.js
@@ -1,6 +1,10 @@
+"use strict";
 // class SessionManager {
 //     constructor() {}
-
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
 //     public async run() {
 //         const sleep = (ms: number): Promise<void> => {
 //             return new Promise(resolve => {
@@ -15,32 +19,21 @@
 //         }
 //     }
 // }
-
 // main
 // const manager = new SessionManager();
 // manager.run();
-
-import express from 'express';
-
-process.on('SIGINT', function() {
+const express_1 = __importDefault(require("express"));
+process.on('SIGINT', function () {
     console.log("CTRL-C: will terminate");
     process.exit();
 });
-process.on('SIGTERM', function() {
-    console.log("SIGTERM: will terminate");
-    process.exit();
-});
-
-const app = express();
+const app = (0, express_1.default)();
 const port = 8080;
-
-const XYZ = '#6'
-
+const XYZ = '#6';
 app.get('/', (req, res) => {
-    console.log(`${new Date().toLocaleTimeString()} - request - ${XYZ}`)
+    console.log(`${new Date().toLocaleTimeString()} - request - ${XYZ}`);
     res.send(`${new Date().toLocaleTimeString()} - Hello - ${XYZ}`);
 });
-
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
 });
