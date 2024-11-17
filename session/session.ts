67a0d3b35a318dff5259c6d550508537a89527b5
Jesper <jkristia@outlook.com>
2024-11-17 11:50:13 -0800

    debug session, this only works with 'inlineSourceMap'

--- session/session.ts
+++ session/session.ts
@@ -1,3 +1,4 @@
+
 class Session {
     constructor(private _id: string) {
 
@@ -16,6 +17,16 @@ class Session {
         }
     }
 }
+
+process.on('SIGINT', function() {
+    console.log("CTRL-C: will terminate");
+    process.exit();
+});
+process.on('SIGTERM', function() {
+    console.log("SIGTERM: will terminate");
+    process.exit();
+});
+
 
 // main
 const session = new Session(new Date().getTime().toString());
