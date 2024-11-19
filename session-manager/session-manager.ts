573eb67671925544dc93f90b39330a9b44bf0902
jesper kristiansen <jkristia@outlook.com>
2024-11-18 20:46:10 -0800

    session manager

--- session-manager/session-manager.ts
+++ session-manager/session-manager.ts
@@ -1,71 +1,116 @@
 import express from 'express';
 import Docker from 'dockerode';
 
-let nextSessionId = 2
-
-const docker = new Docker({ socketPath: '/var/run/docker.sock' });
-const createdContainers: Docker.Container[] = [];
-async function listAndStartContainers() {
-    try {
-        const networks = await docker.listNetworks();
-        const network = networks.find( n => n.Name.endsWith('my_network'))
-        // List only running containers
-        const containers = await docker.listContainers({ all: false });
-        for (const containerInfo of containers) {
-            console.log(`ID     : ${containerInfo.Id}`);
-            console.log(`  Name : ${containerInfo.Names.join(', ')}`);
-            console.log(`  Image: ${containerInfo.Image}`);
-            console.log(`  State: ${containerInfo.State}`);
-        }
-        const newcontainer = await docker.createContainer({
-            Image: 'session-run:latest',
-            name: `session-${nextSessionId}`,
-            Cmd: ['node', './session/session.js', `port=${10100 + nextSessionId}`, `session-id=${nextSessionId}`],
-            HostConfig: {
-                NetworkMode: network?.Name
-            }
-        });
-        nextSessionId++;
-        createdContainers.push(newcontainer);
-        await newcontainer.start();
-        console.log('Started a new instance of the container:', newcontainer.id);
-    } catch (error) {
-        console.error('Error:', error);
+class Session {
+    public get id(): number {
+        return this._id;
+    }
+    public get containerId(): string {
+        return this._info.Id;
+    }
+    public get name(): string {
+        return this._info.Names[0]
+    }
+    public get container(): Docker.Container | undefined {
+        return this._container;
+    }
+    constructor(
+        private _id: number,
+        private _info: Docker.ContainerInfo,
+        private _container?: Docker.Container
+    ) {
     }
 }
 
-async function stopAllCreatedContainers() {
-    for (const container of createdContainers) {
+class SessionManager {
+    private fixedSessionName = 'session-1';
+    private sessionImage = 'session-run:latest';
+    private _nextId = 1;
+    private _sessions: Session[] = [];
+    private _docker = new Docker({ socketPath: '/var/run/docker.sock' });
+    constructor() {
+        this.addInitialSession()
+    }
+
+    private async addInitialSession() {
+        const container = await this.findContainer(this.fixedSessionName);
+        if (container) {
+            this._sessions.push(new Session(1, container));
+            this._nextId = 2;
+        }
+    }
+    public async findContainer(name: string): Promise<Docker.ContainerInfo | undefined> {
+        const containers = await this._docker.listContainers({ all: false });
+        const container = containers.find(c => c.Names[0].endsWith(name))
+        if (!container) {
+            console.log(`container NOT found ${name}, available containers:`)
+            containers.forEach(c => {
+                console.log(` - '${c.Names.join(",")}'`)
+            })
+        }
+        // console.log(container)
+        return container;
+    }
+    public async createSession() {
         try {
-            await container.stop();
-            await container.remove();
-            console.log('Stopped container:', container.id);
+            const networks = await this._docker.listNetworks();
+            const network = networks.find(n => n.Name.endsWith('sys-network'))
+            const sessionId = this._nextId++;
+            const sessionContainerName = `session-${sessionId}`;
+            const newcontainer = await this._docker.createContainer({
+                Image: this.sessionImage,
+                name: sessionContainerName,
+                Cmd: ['node', './session/session.js', `port=${10010 + sessionId}`, `session-id=${sessionId}`],
+                HostConfig: {
+                    NetworkMode: network?.Name
+                }
+            });
+            await newcontainer.start();
+            const info = await this.findContainer(sessionContainerName)
+            this._sessions.push(new Session(sessionId, info!, newcontainer))
+            console.log('Started new session:', sessionId);
         } catch (error) {
-            console.error('Failed to stop container:', container.id, error);
+            console.error('Error:', error);
+        }
+    }
+    public async shutdown() {
+        for (const session of this._sessions) {
+            if (!session.container) {
+                continue;
+            }
+            try {
+                await session.container.stop();
+                await session.container.remove();
+                console.log('Stopped session:', session.id);
+            } catch (error) {
+                console.error('Failed to stop container:', session.id, session.container.id, error);
+            }
         }
     }
 }
+
+const manager = new SessionManager()
 
 process.on('SIGINT', async () => {
     console.log("CTRL-C: will terminate");
-    await stopAllCreatedContainers();
+    await manager.shutdown();
     process.exit();
 });
 process.on('SIGTERM', async () => {
     console.log("SIGTERM: will terminate");
-    await stopAllCreatedContainers();
+    await manager.shutdown();
     process.exit();
 });
+
+
 
 const app = express();
 const port = 8080;
 
-const XYZ = '#7'
-
 app.get('/', (req, res) => {
-    listAndStartContainers()
-    console.log(`${new Date().toLocaleTimeString()} - request - ${XYZ}`)
-    res.send(`${new Date().toLocaleTimeString()} - Hello - ${XYZ}`);
+    manager.createSession()
+    console.log(`${new Date().toLocaleTimeString()} - request`)
+    res.send(`${new Date().toLocaleTimeString()} - Hello`);
 });
 
 app.listen(port, () => {
